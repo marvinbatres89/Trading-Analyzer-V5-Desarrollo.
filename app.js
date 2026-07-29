b3d81ef6aea8096aa230fe9d4d24e73c64db16b2
marvinbatres89 <marvinbatres17@gmail.com>
2026-07-29 14:04:13 -0600

    Corregir cierre de comentario en app.js

--- app.js
+++ app.js
@@ -2273,6 +2273,7 @@ FIN DE LA PARTE 2 DE 4
 NO BORRES ESTA LÍNEA.
 LA PARTE 3 DEBE PEGARSE INMEDIATAMENTE DEBAJO.
 =========================================================
+*/
 
 /*
 =========================================================
